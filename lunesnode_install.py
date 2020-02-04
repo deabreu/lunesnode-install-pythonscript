ae2c8149b332caa2c67abc3947e213fbc5ecb0fa
Ronaldo Alves de Abreu <deabreu.ronaldo@gmail.com>
2020-02-04 14:42:59 -0300

    Create lunesnode_install.py
    initial installation script

---
+++ lunesnode_install.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+
+class SudoValidator(object):
+    def __init__(self):
+        pass
+    def checkAdmin(self):
+        pass
+    def testDirectoryCreation(self):
+        pass
+
+class DirectoryHandler(object):
+    def __init__(self):
+        pass
+    def make(self, path):
+        pass
+    def list(self):
+        pass
+    def destroy(self):
+        pass
+
+class OnlineResource(object):
+    def __init__(self):
+        pass
+
+class GithubResource(OnlineResource):
+    def __init__(self):
+        pass
+
+class CreateFileHandler(object):
+    def __init__(self):
+        pass
+
+class ConfigurationFileHandler(CreateFileHandler):
+    def __init__(self):
+        pass
+
+class ServiceFileHandler(CreateFileHandler):
+    def __init__(self):
+        pass
+
+if __name__ = "__main__":
+    #run this
+    pass
